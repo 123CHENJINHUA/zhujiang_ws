b0eb2c5563dbbd63d80bedd9f68eddcd7f6b4eb4
CJH <jchenjb@connect.ust.hk>
2025-07-04 16:29:45 +0800

    更换ros_tracer包

--- src/tracer_ros/tracer_base/include/tracer_base/tracer_messenger.hpp
+++ src/tracer_ros/tracer_base/include/tracer_base/tracer_messenger.hpp
@@ -18,7 +18,6 @@
 
 #include "tracer_msgs/TracerLightCmd.h"
 #include "ugv_sdk/mobile_robot/tracer_robot.hpp"
-#include "tracer_msgs/TracerRsStatus.h"
 namespace westonrobot
 {
 
@@ -52,7 +51,6 @@ private:
 
     ros::Publisher odom_publisher_;
     ros::Publisher status_publisher_;
-    ros::Publisher rs_status_publisher_;
     ros::Publisher status_uart_publisher_;
     ros::Subscriber motion_cmd_subscriber_;
     ros::Subscriber light_cmd_subscriber_;
